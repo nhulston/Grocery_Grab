ec3ea2294c5b270d2c02475ef790cb2b1b221c4a
Joseph Vitko <josephvitko21@gmail.com>
2020-10-17 23:48:27 -0400

    finished map

--- app/src/main/java/com/smithbois/grocerygrab/util/map/DrawView.java
+++ app/src/main/java/com/smithbois/grocerygrab/util/map/DrawView.java
@@ -5,10 +5,17 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.smithbois.grocerygrab.R;
+import com.smithbois.grocerygrab.demo.DemoProductList;
+import com.smithbois.grocerygrab.demo.DemoStoreMap;
+import com.smithbois.grocerygrab.util.pathfinding.Driver;
+import com.smithbois.grocerygrab.util.pathfinding.Product;
+
+import java.util.List;
 
 public class DrawView extends View {
     Path path = new Path();
@@ -40,27 +47,55 @@ public class DrawView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+
+        List<Point> bestPoints = Driver.optimizeRoute(DemoStoreMap.map(), DemoProductList.list());
+        System.out.println("best points: " + bestPoints);
+
+        float startx = 100f;
+        float starty = 200f;
+        float width = 800f;
+        float height = 800f;
+
         path.reset();
         float[] radii = new float[]{
                 25f, 25f,
                 25f, 25f,
-                25f, 25f,
-                25f, 25f
+                0f, 0f,
+                0f, 0f
         };
-        path.addRoundRect(100f, 200f, 900f, 900f, radii, Path.Direction.CW);
+        path.addRoundRect(startx, starty, startx+width, starty+height, radii, Path.Direction.CW);
         //canvas.drawPath(path, fillPaint);
         canvas.drawPath(path, strokePaint);
         strokePaint.setStrokeWidth(10);
         path.reset();
-        path.addRoundRect(250f, 300f, 350f, 800f, radii, Path.Direction.CW);
+        path.addRoundRect(startx+width*1/5-35, 350f, startx+width*1/5+35, 1000f, radii, Path.Direction.CW);
         canvas.drawPath(path, fillPaint);
         path.reset();
-        path.addRoundRect(450f, 300f, 550f, 800f, radii, Path.Direction.CW);
+        path.addRoundRect(startx+width*2/5-35, 350f, startx+width*2/5+35, 1000f, radii, Path.Direction.CW);
         canvas.drawPath(path, fillPaint);
         path.reset();
-        path.addRoundRect(650f, 300f, 750f, 800f, radii, Path.Direction.CW);
+        path.addRoundRect(startx+width*3/5-35, 350f, startx+width*3/5+35, 1000f, radii, Path.Direction.CW);
         canvas.drawPath(path, fillPaint);
         path.reset();
+        path.addRoundRect(startx+width*4/5-35, 350f, startx+width*4/5+35, 1000f, radii, Path.Direction.CW);
+        canvas.drawPath(path, fillPaint);
+        path.reset();
+
+        strokePaint.setColor(Color.RED);
+        float sizeAdjustmentFactor = 1.25f;
+        for(int i = 0; i < bestPoints.size()-1; i++){
+            canvas.drawLine(((float) bestPoints.get(i).y-0) * width/(DemoStoreMap.map()[0].length-1) + startx,
+                    ((float) bestPoints.get(i).x-0) * height/(DemoStoreMap.map().length-1) + starty,
+                    ((float) bestPoints.get(i+1).y-0) * width/(DemoStoreMap.map()[0].length-1) + startx,
+                    ((float) bestPoints.get(i+1).x-0) * height/(DemoStoreMap.map().length-1) + starty,
+                    strokePaint
+                    );
+        }
+
+        fillPaint.setColor(Color.BLUE);
+        for(Product p : DemoProductList.list()){
+            canvas.drawCircle(p.getY() * width/(DemoStoreMap.map()[0].length-1) + startx, p.getX() * height/(DemoStoreMap.map().length-1) + starty, 10, fillPaint);
+        }
 
         //canvas.drawLine(0, 0, 100, 100, paint);
         //canvas.drawLine(100, 0, 0, 100, paint);
