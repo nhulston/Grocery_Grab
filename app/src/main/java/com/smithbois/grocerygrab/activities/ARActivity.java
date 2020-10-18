71b71b05cc175caf478964dddb3418228f2cc747
Joseph Vitko <josephvitko21@gmail.com>
2020-10-17 20:57:15 -0400

    changed local rotation to world

--- app/src/main/java/com/smithbois/grocerygrab/activities/ARActivity.java
+++ app/src/main/java/com/smithbois/grocerygrab/activities/ARActivity.java
@@ -72,7 +72,8 @@ public class ARActivity extends AppCompatActivity {
 
     private void createModel(AnchorNode anchorNode) {
         TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
-        node.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), 90f));
+        //node.setLocalRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), 90f));
+        node.setWorldRotation(Quaternion.axisAngle(new Vector3(1f, 0, 0), 90f));
         node.setParent(anchorNode);
         node.setRenderable(modelRenderable);
         node.select();
@@ -92,6 +93,7 @@ public class ARActivity extends AppCompatActivity {
             Node arrow = new Node();
             arrow.setParent(anchorNode);
             arrow.setRenderable(arrowRenderable);
+            //arrow.setWorldRotation(new Quaternion(new Vector3()));
             placed = true;
         }
     }
